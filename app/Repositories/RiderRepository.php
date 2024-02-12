7906d003ad17bcbe2ec58e183b44c90b92ab46ca
Kalyan Brata Chakraborty <amitchakraborty147@gmail.com>
2024-02-12 17:18:16 +0600

    added duration

--- app/Repositories/RiderRepository.php
+++ app/Repositories/RiderRepository.php
@@ -19,8 +19,8 @@ class RiderRepository
     {
         return DB::select(DB::raw("SELECT id, service_name, lat, longitude, capture_name, SQRT(
                 POW(69.1 * (lat - :startLat), 2) +
-                POW(69.1 * (:startLong - longitude) * COS(lat / 57.3), 2)) AS distance
-                FROM riders ORDER BY distance;"
+                POW(69.1 * (:startLong - longitude) * COS(lat / 57.3), 2)) AS distance, timestamp
+                FROM riders HAVING distance < 40 OR timestamp BETWEEN (DATE_SUB(NOW(),INTERVAL 5 MINUTE)) AND NOW() ORDER BY distance;"
         ), [
             'startLat' => $restaurant_lat,
             'startLong' => $restaurant_long
